28e68d5daaf67b2cffba6b266f27f4b29d04f59a
Hrvoje <hrvoje.zeman1@gmail.com>
2025-04-10 18:47:45 +0200

    First version of the cancer software

---
+++ NN.py
@@ -0,0 +1,44 @@
+# This Python 3 environment comes with many helpful analytics libraries installed
+# It is defined by the kaggle/python Docker image: https://github.com/kaggle/docker-python
+# For example, here's several helpful packages to load
+
+#OS libs
+import os
+import shutil
+import itertools
+import pathlib
+from PIL import Image
+
+#Data handling tools
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+sns.set_style('whitegrid')
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix , classification_report
+
+#Deep learning libs
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.layers import Sequential
+from tensorflow.keras.layers import Conv2D , MaxPooling2D , Flatten , Activation , Dense , Dropout , BatchNormalization
+from tensorflow.keras.optimizers import Adam , Adamax
+from tensorflow.keras import regularizers
+
+
+img_shape = (-1, -1, -1)
+num_class = -1
+
+base_model = tf.keras.applications.efficientnet.EfficientNetB7(include_top = False, weights = 'imagenet', input_shape = img_shape, pooling= 'max')
+model = Sequential([
+    base_model,
+    BatchNormalization(axis= -1 , momentum= 0.99 , epsilon= 0.001),
+    Dense(256, kernel_regularizer = regularizers.l2(l= 0.016) , activity_regularizer = regularizers.l1(0.006), bias_regularizer= regularizers.l1(0.006) , activation = 'relu'),
+    Dropout(rate= 0.4 , seed = 75),
+    Dense(num_class, activation = 'softmax')
+])
+
+model.compile(Adamax(learning_rate = 0.001) , loss = 'categorical_crossentropy', metrics = ['accuracy'])
+model.summary()
